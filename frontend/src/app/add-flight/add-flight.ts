5787439402d7cac0084c7d2ba28107a18cc2d4e0
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-21 16:23:16 +0530

    added 'add-flight'

---
+++ frontend/src/app/add-flight/add-flight.ts
@@ -0,0 +1,54 @@
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FlightService } from '../_services/flight';
+@Component({
+  selector: 'app-add-flight',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './add-flight.html',
+  styleUrls: ['./add-flight.css']
+})
+export class AddFlightComponent {
+  flight: any = {
+    fromPlace: '',
+    toPlace: '',
+    departureTime: '',
+    arrivalTime: '',
+    availableSeats: 60,
+    airlineId: '',
+    flightNumber: '',
+    price: {
+      oneWay: 0,
+      roundTrip: 0
+    }
+  };
+  isSuccessful = false;
+  errorMessage = '';
+  constructor(
+    private flightService: FlightService,
+    private router: Router,
+    private cd: ChangeDetectorRef
+  ) {}
+  onSubmit(): void {
+    console.log('Submitting Flight:', this.flight);
+
+    this.flightService.addFlight(this.flight).subscribe({
+      next: (data) => {
+        this.isSuccessful = true;
+        this.errorMessage = '';
+        this.cd.detectChanges();        
+        setTimeout(() => {
+           this.router.navigate(['/search']); 
+        }, 2000);
+      },
+      error: (err) => {
+        console.error(err);
+        this.errorMessage = err.error?.message || 'Error adding flight';
+        this.isSuccessful = false;
+        this.cd.detectChanges();
+      }
+    });
+  }
+}

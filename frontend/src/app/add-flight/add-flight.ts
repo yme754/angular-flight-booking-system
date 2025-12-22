09abdac46ecf76c16302c4f91e03f3e87e212421
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-22 10:53:09 +0530

    disabled past dates and time selection in add-flight

--- frontend/src/app/add-flight/add-flight.ts
+++ frontend/src/app/add-flight/add-flight.ts
@@ -17,7 +17,7 @@ export class AddFlightComponent {
     toPlace: '',
     departureTime: '',
     arrivalTime: '',
-    availableSeats: 60, // Better default than 0
+    availableSeats: 0,
     airlineId: '',
     flightNumber: '',
     price: {
@@ -25,7 +25,7 @@ export class AddFlightComponent {
       roundTrip: 0
     }
   };
-  
+  minDateTime: string = '';
   isSuccessful = false;
   errorMessage = '';
 
@@ -33,24 +33,33 @@ export class AddFlightComponent {
     private flightService: FlightService,
     private router: Router,
     private cd: ChangeDetectorRef
-  ) {}
+  ) {
+      const now = new Date();
+      this.minDateTime = now.toISOString().slice(0,16);
+  }
   validateForm(): boolean {
     this.errorMessage = '';
+    const dep = new Date(this.flight.departureTime); 
+    const arr = new Date(this.flight.arrivalTime);
+    if (dep < new Date()) { 
+      this.errorMessage = 'Departure time cannot be in the past.'; 
+      return false; 
+    }
+    if(arr < new Date()) {
+      this.errorMessage = 'Departure time cannot be in the past.';
+      return false;
+    }
     if (this.flight.availableSeats <= 0) {
       this.errorMessage = 'Available seats must be greater than 0.';
       return false;
     }
+    if (arr <= dep) { 
+      this.errorMessage = 'Arrival time must be later than Departure time.'; 
+      return false; 
+    }
     if (this.flight.price.oneWay <= 0 || this.flight.price.roundTrip <= 0) {
       this.errorMessage = 'Prices must be greater than 0.';
       return false;
-    }
-    if (this.flight.departureTime && this.flight.arrivalTime) {
-      const dep = new Date(this.flight.departureTime);
-      const arr = new Date(this.flight.arrivalTime);
-      if (arr <= dep) {
-        this.errorMessage = 'Arrival time must be later than Departure time.';
-        return false;
-      }
     }
     return true;
   }

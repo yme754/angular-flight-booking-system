6cd8059a8b87bbb0b122556f36f65b777d7a07be
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-24 14:20:53 +0530

    Revert "storing only frontend in this repo"
    This reverts commit fa0f72621887e55f5a9897908f5224d753826ccf.

---
+++ backend/flight-service/src/test/java/com/flightapp/FlightServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.flightapp;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled("Skipping context load check")
+class FlightServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
